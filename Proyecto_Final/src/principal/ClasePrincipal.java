a8f26a40d3129ac33612f19e482502bffbfa8b11
alvarodans <adansmon@everis.com>
2021-04-28 18:46:02 +0200

    sebas aprende

--- Proyecto_Final/src/principal/ClasePrincipal.java
+++ Proyecto_Final/src/principal/ClasePrincipal.java
@@ -4,7 +4,7 @@ public class ClasePrincipal {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Prueba");
+		System.out.println("Sebas aprende a hacer commit and push");
 
 	}
 
