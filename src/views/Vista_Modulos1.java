896e4d76a9ed1c9bb0e0f93003c9fac671aea33a
Alvaro-Dans <alvarodans@hotmail.com>
2021-05-30 11:29:51 +0200

    pruebas

--- src/views/Vista_Modulos1.java
+++ src/views/Vista_Modulos1.java
@@ -6,121 +6,134 @@ import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
+import javax.swing.border.EmptyBorder;
+
+import java.awt.Color;
 
 public class Vista_Modulos1 extends JPanel {
 
 	public Vista_Modulos1() {
+		
 		setLayout(new BorderLayout(0, 0));
 		
 		JPanel panel_login = new JPanel();
+		panel_login.setBackground(new Color(255, 204, 153));
 		FlowLayout fl_panel_login = (FlowLayout) panel_login.getLayout();
 		fl_panel_login.setAlignment(FlowLayout.RIGHT);
 		add(panel_login, BorderLayout.NORTH);
 		
-		JLabel lblNewLabel = new JLabel("New label");
-		panel_login.add(lblNewLabel);
+		JLabel lblUsuario = new JLabel("New label");
+		lblUsuario.setBorder(new EmptyBorder(0,0,0,10));
+		panel_login.add(lblUsuario);
 		
-		JButton btnNewButton = new JButton("New button");
-		panel_login.add(btnNewButton);
+		JButton btnLogout = new JButton("");
+		btnLogout.setIcon(new ImageIcon(".\\images\\logout.png"));
+		btnLogout.setBorder(null);
+		btnLogout.setBackground(null);
+		panel_login.add(btnLogout);
 		
-		JPanel panel = new JPanel();
-		add(panel, BorderLayout.CENTER);
-		panel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		JPanel panel_principal = new JPanel();
+		panel_principal.setBackground(new Color(255, 204, 153));
+		add(panel_principal, BorderLayout.CENTER);
+		panel_principal.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
-		JPanel panel_1 = new JPanel();
-		panel.add(panel_1);
-		GridBagLayout gbl_panel_1 = new GridBagLayout();
-		gbl_panel_1.columnWidths = new int[]{0, 0, 0, 0};
-		gbl_panel_1.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0};
-		gbl_panel_1.columnWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gbl_panel_1.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		panel_1.setLayout(gbl_panel_1);
+		JPanel panel_modulos = new JPanel();
+		panel_modulos.setBackground(new Color(255, 204, 153));
+		panel_principal.add(panel_modulos);
+		GridBagLayout gbl_panel_modulos = new GridBagLayout();
+		gbl_panel_modulos.columnWidths = new int[]{0, 0, 0, 0};
+		gbl_panel_modulos.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_panel_modulos.columnWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel_modulos.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		panel_modulos.setLayout(gbl_panel_modulos);
 		
-		JButton btnNewButton_4 = new JButton("");
-		btnNewButton_4.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
 		
-		JLabel lblNewLabel_3 = new JLabel("M O D U L O S");
-		lblNewLabel_3.setFont(new Font("NSimSun", Font.PLAIN, 90));
-		GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
-		gbc_lblNewLabel_3.gridwidth = 2;
-		gbc_lblNewLabel_3.insets = new Insets(60, 0, 5, 5);
-		gbc_lblNewLabel_3.gridx = 0;
-		gbc_lblNewLabel_3.gridy = 0;
-		panel_1.add(lblNewLabel_3, gbc_lblNewLabel_3);
-		btnNewButton_4.setIcon(new ImageIcon(".\\images\\clientes 1.png"));
-		GridBagConstraints gbc_btnNewButton_4 = new GridBagConstraints();
-		gbc_btnNewButton_4.insets = new Insets(50, 100, 5, 100);
-		gbc_btnNewButton_4.gridx = 0;
-		gbc_btnNewButton_4.gridy = 1;
-		panel_1.add(btnNewButton_4, gbc_btnNewButton_4);
+		JLabel lblTitulo = new JLabel("M O D U L O S");
+		lblTitulo.setFont(new Font("NSimSun", Font.PLAIN, 90));
+		GridBagConstraints gbc_lblTitulo = new GridBagConstraints();
+		gbc_lblTitulo.gridwidth = 2;
+		gbc_lblTitulo.insets = new Insets(60, 0, 5, 5);
+		gbc_lblTitulo.gridx = 0;
+		gbc_lblTitulo.gridy = 0;
+		panel_modulos.add(lblTitulo, gbc_lblTitulo);
 		
-		JButton btnNewButton_5 = new JButton("");
-		btnNewButton_5.setIcon(new ImageIcon(".\\images\\marketing 1.png"));
-		btnNewButton_5.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		GridBagConstraints gbc_btnNewButton_5 = new GridBagConstraints();
-		gbc_btnNewButton_5.insets = new Insets(50, 100, 5, 100);
-		gbc_btnNewButton_5.gridx = 1;
-		gbc_btnNewButton_5.gridy = 1;
-		panel_1.add(btnNewButton_5, gbc_btnNewButton_5);
 		
-		JLabel lblNewLabel_4 = new JLabel("New label");
-		GridBagConstraints gbc_lblNewLabel_4 = new GridBagConstraints();
-		gbc_lblNewLabel_4.insets = new Insets(0, 0, 100, 5);
-		gbc_lblNewLabel_4.gridx = 0;
-		gbc_lblNewLabel_4.gridy = 2;
-		panel_1.add(lblNewLabel_4, gbc_lblNewLabel_4);
+		JButton btnClientes = new JButton("");
+		btnClientes.setIcon(new ImageIcon(".\\images\\clientes 1.png"));
+		GridBagConstraints gbc_btnClientes = new GridBagConstraints();
+		gbc_btnClientes.insets = new Insets(50, 100, 5, 100);
+		gbc_btnClientes.gridx = 0;
+		gbc_btnClientes.gridy = 1;
+		btnClientes.setBorder(null);
+		btnClientes.setBackground(null);
+		panel_modulos.add(btnClientes, gbc_btnClientes);
 		
-		JLabel lblNewLabel_5 = new JLabel("New label");
-		GridBagConstraints gbc_lblNewLabel_5 = new GridBagConstraints();
-		gbc_lblNewLabel_5.insets = new Insets(0, 0, 100, 5);
-		gbc_lblNewLabel_5.gridx = 1;
-		gbc_lblNewLabel_5.gridy = 2;
-		panel_1.add(lblNewLabel_5, gbc_lblNewLabel_5);
 		
-		JButton btnNewButton_3 = new JButton("");
-		btnNewButton_3.setIcon(new ImageIcon(".\\images\\factura 1.png"));
-		GridBagConstraints gbc_btnNewButton_3 = new GridBagConstraints();
-		gbc_btnNewButton_3.insets = new Insets(0, 0, 5, 5);
-		gbc_btnNewButton_3.gridx = 0;
-		gbc_btnNewButton_3.gridy = 5;
-		panel_1.add(btnNewButton_3, gbc_btnNewButton_3);
+		JButton btnMarketing = new JButton("");
+		btnMarketing.setIcon(new ImageIcon(".\\images\\marketing 1.png"));
+		btnMarketing.setBorder(null);
+		GridBagConstraints gbc_btnMarketing = new GridBagConstraints();
+		gbc_btnMarketing.insets = new Insets(50, 100, 5, 100);
+		gbc_btnMarketing.gridx = 1;
+		gbc_btnMarketing.gridy = 1;
+		btnMarketing.setBackground(null);
+		panel_modulos.add(btnMarketing, gbc_btnMarketing);
 		
-		JButton btnNewButton_2 = new JButton("");
-		btnNewButton_2.setIcon(new ImageIcon(".\\images\\programar 1.png"));
-		GridBagConstraints gbc_btnNewButton_2 = new GridBagConstraints();
-		gbc_btnNewButton_2.insets = new Insets(0, 0, 5, 5);
-		gbc_btnNewButton_2.gridx = 1;
-		gbc_btnNewButton_2.gridy = 5;
-		panel_1.add(btnNewButton_2, gbc_btnNewButton_2);
 		
-		JLabel lblNewLabel_1 = new JLabel("New label");
-		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
-		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNewLabel_1.gridx = 0;
-		gbc_lblNewLabel_1.gridy = 6;
-		panel_1.add(lblNewLabel_1, gbc_lblNewLabel_1);
+		JLabel lblClientes = new JLabel("Clientes");
+		GridBagConstraints gbc_lblClientes = new GridBagConstraints();
+		gbc_lblClientes.insets = new Insets(0, 0, 100, 5);
+		gbc_lblClientes.gridx = 0;
+		gbc_lblClientes.gridy = 2;
+		panel_modulos.add(lblClientes, gbc_lblClientes);
 		
-		JLabel lblNewLabel_2 = new JLabel("New label");
-		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
-		gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNewLabel_2.gridx = 1;
-		gbc_lblNewLabel_2.gridy = 6;
-		panel_1.add(lblNewLabel_2, gbc_lblNewLabel_2);
+		
+		JLabel lblMarketing = new JLabel("Marketing");
+		GridBagConstraints gbc_lblMarketing = new GridBagConstraints();
+		gbc_lblMarketing.insets = new Insets(0, 0, 100, 5);
+		gbc_lblMarketing.gridx = 1;
+		gbc_lblMarketing.gridy = 2;
+		panel_modulos.add(lblMarketing, gbc_lblMarketing);
+		
+		
+		JButton btnFacturas = new JButton("");
+		btnFacturas.setIcon(new ImageIcon(".\\images\\factura 1.png"));
+		GridBagConstraints gbc_btnFacturas = new GridBagConstraints();
+		gbc_btnFacturas.insets = new Insets(0, 0, 5, 5);
+		gbc_btnFacturas.gridx = 0;
+		gbc_btnFacturas.gridy = 5;
+		btnFacturas.setBorder(null);
+		btnFacturas.setBackground(null);
+		panel_modulos.add(btnFacturas, gbc_btnFacturas);
+		
+		
+		JButton btnProgramar = new JButton("");
+		btnProgramar.setIcon(new ImageIcon(".\\images\\programar 1.png"));
+		GridBagConstraints gbc_btnProgramar = new GridBagConstraints();
+		gbc_btnProgramar.insets = new Insets(0, 0, 5, 5);
+		gbc_btnProgramar.gridx = 1;
+		gbc_btnProgramar.gridy = 5;
+		btnProgramar.setBorder(null);
+		btnProgramar.setBackground(null);
+		panel_modulos.add(btnProgramar, gbc_btnProgramar);
+		
+		JLabel lblFacturas = new JLabel("Facturas");
+		GridBagConstraints gbc_lblFacturas = new GridBagConstraints();
+		gbc_lblFacturas.insets = new Insets(0, 0, 5, 5);
+		gbc_lblFacturas.gridx = 0;
+		gbc_lblFacturas.gridy = 6;
+		panel_modulos.add(lblFacturas, gbc_lblFacturas);
+		
+		JLabel lblProgramar = new JLabel("Programar");
+		GridBagConstraints gbc_lblProgramar = new GridBagConstraints();
+		gbc_lblProgramar.insets = new Insets(0, 0, 5, 5);
+		gbc_lblProgramar.gridx = 1;
+		gbc_lblProgramar.gridy = 6;
+		panel_modulos.add(lblProgramar, gbc_lblProgramar);
 	}
 	
 }
