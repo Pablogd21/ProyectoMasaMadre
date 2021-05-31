26f6caa4c4941bcb3657bd2ea8baa69fef798fe7
Sebastian Gomez <Sebastian110499@gmail.com>
2021-05-31 08:28:48 +0200

    Vista login

--- src/views/Vista_Login.java
+++ src/views/Vista_Login.java
@@ -2,8 +2,16 @@ package views;
 
 import javax.swing.JPanel;
 import javax.swing.JLabel;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.GridBagLayout;
+import java.awt.Toolkit;
+
 import javax.swing.JTextField;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 public class Vista_Login extends JPanel{
@@ -13,33 +21,44 @@ public class Vista_Login extends JPanel{
 	private JTextField txtUsuario;
 	
 	public Vista_Login() {
+		
+		Toolkit miPantalla = Toolkit.getDefaultToolkit();
+		Dimension tamanyoPantalla = miPantalla.getScreenSize();
+		int altoPantalla = tamanyoPantalla.height;
+		int anchoPantalla = tamanyoPantalla.width;
+		setSize(1920, 1080);
 		setLayout(null);
 		
-		this.setSize(750, 430 );
+		JLabel logo = new JLabel();
+		logo.setIcon(new ImageIcon(".\\images\\Logo.jpg"));
+		logo.setBounds(192, 70, 743, 632);
+		add(logo);
 		
 		JLabel lblUsuario = new JLabel("Usuario: ");
 		lblUsuario.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 18));
-		lblUsuario.setBounds(418, 106, 196, 22);
+		lblUsuario.setBounds(945, 138, 81, 22);
 		add(lblUsuario);
 		
 		txtUsuario = new JTextField();
-		txtUsuario.setBounds(418, 139, 241, 22);
+		txtUsuario.setBounds(955, 181, 268, 20);
 		add(txtUsuario);
 		txtUsuario.setColumns(10);
 		
 		JLabel lblPassword = new JLabel("Contrase\u00F1a: ");
 		lblPassword.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 18));
-		lblPassword.setBounds(418, 196, 196, 22);
+		lblPassword.setBounds(945, 252, 116, 22);
 		add(lblPassword);
 		
 		txtPassword = new JTextField();
 		txtPassword.setColumns(10);
-		txtPassword.setBounds(418, 229, 241, 22);
+		txtPassword.setBounds(955, 299, 268, 20);
 		add(txtPassword);
 		
 		btnIngresar = new JButton("Ingresar");
 		btnIngresar.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 16));
-		btnIngresar.setBounds(445, 276, 196, 34);
+		btnIngresar.setBounds(996, 379, 197, 29);
 		add(btnIngresar);
+		
+		
 	}
 }
