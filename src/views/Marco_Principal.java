5650bf95d5a1cc70ad7bca289dbae626a06bb6fb
Alvaro-Dans <alvarodans@hotmail.com>
2021-06-01 20:11:43 +0200

    pruebas fer

--- src/views/Marco_Principal.java
+++ src/views/Marco_Principal.java
@@ -10,7 +10,7 @@ public class Marco_Principal extends JFrame {
 	public Marco_Principal() {
 		setTitle("hola");
 		setExtendedState(MAXIMIZED_BOTH);
-		Vista_Modulos1 vistaM = new Vista_Modulos1();
+		Vista_Marketing1 vistaM = new Vista_Marketing1();
 		add(vistaM);
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
--- src/views/Vista_Marketing.java
+++ src/views/Vista_Marketing.java
@@ -1,7 +1,10 @@
 package views;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.GridLayout;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -10,91 +13,143 @@ import javax.swing.JTextField;
 
 public class Vista_Marketing extends JPanel {
 	
-	private JButton btnAniadirUsu;
-	private JButton btnModificar;
 	private JButton btnEliminar;
 	private JButton btnResumen;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
 	private JTextField textField_3;
 	private JTextField textField_4;
-	private JTextField textField_5;
 	
 	public Vista_Marketing() {
-		setLayout(null);
 		
-		this.setSize(750, 430 );
+		this.setSize(1920, 1080);
+		setLayout(new BorderLayout(0, 0));
 		
-		btnAniadirUsu = new JButton("Nueva Comunicación");
-		btnAniadirUsu.setBackground(Color.BLACK);
+		JPanel panelBotones = new JPanel();
+		add(panelBotones, BorderLayout.WEST);
+		panelBotones.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 150));
+		
+		JPanel panel = new JPanel();
+		panelBotones.add(panel);
+		panel.setLayout(new GridLayout(0, 1, 0, 5));
+		
+		JButton btnAniadirUsu = new JButton("Nueva Comunicaci\u00F3n");
 		btnAniadirUsu.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 15));
-		btnAniadirUsu.setBounds(23, 147, 175, 28);
-		add(btnAniadirUsu);
+		btnAniadirUsu.setBackground(Color.BLACK);
+		panel.add(btnAniadirUsu);
 		
-		btnModificar = new JButton("Felicitaciones");
-		btnModificar.setBackground(Color.BLACK);
+		JButton btnModificar = new JButton("Felicitaciones");
 		btnModificar.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 15));
-		btnModificar.setBounds(23, 186, 175, 28);
-		add(btnModificar);
+		btnModificar.setBackground(Color.BLACK);
+		panel.add(btnModificar);
 		
 		JButton btnModificar_1 = new JButton("Encuestas");
 		btnModificar_1.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 15));
 		btnModificar_1.setBackground(Color.BLACK);
-		btnModificar_1.setBounds(23, 224, 175, 28);
-		add(btnModificar_1);
+		panel.add(btnModificar_1);
 		
-		JPanel panel = new JPanel();
-		panel.setLayout(null);
-		panel.setBounds(208, 25, 526, 348);
-		add(panel);
+		JPanel panelCambiante = new JPanel();
+		add(panelCambiante);
+		panelCambiante.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panelFelicitacion = new JPanel();
+		panelFelicitacion.setLayout(null);
+		panelCambiante.add(panelFelicitacion);
+		
+		JLabel lblFelicitacion = new JLabel("FELICITACION:");
+		lblFelicitacion.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lblFelicitacion.setBounds(190, 10, 142, 22);
+		panelFelicitacion.add(lblFelicitacion);
+		
+		JLabel lblTitulo_1 = new JLabel("TITULO:");
+		lblTitulo_1.setToolTipText("");
+		lblTitulo_1.setFont(new Font("Cambria Math", Font.PLAIN, 14));
+		lblTitulo_1.setBounds(20, 55, 82, 28);
+		panelFelicitacion.add(lblTitulo_1);
+		
+		JLabel lblContenido_1 = new JLabel("CONTENIDO:");
+		lblContenido_1.setToolTipText("");
+		lblContenido_1.setFont(new Font("Cambria Math", Font.PLAIN, 14));
+		lblContenido_1.setBounds(10, 104, 82, 28);
+		panelFelicitacion.add(lblContenido_1);
+		
+		textField = new JTextField();
+		textField.setColumns(10);
+		textField.setBounds(95, 55, 404, 28);
+		panelFelicitacion.add(textField);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(95, 93, 404, 203);
+		panelFelicitacion.add(textField_1);
+		
+		JButton btnGuardar_2 = new JButton("GUARDAR");
+		btnGuardar_2.setFont(new Font("Tahoma", Font.BOLD, 15));
+		btnGuardar_2.setBackground(Color.BLACK);
+		btnGuardar_2.setBounds(95, 311, 158, 28);
+		panelFelicitacion.add(btnGuardar_2);
+		
+		JButton btnCancelar_1 = new JButton("BORRAR");
+		btnCancelar_1.setFont(new Font("Tahoma", Font.BOLD, 15));
+		btnCancelar_1.setBackground(Color.BLACK);
+		btnCancelar_1.setBounds(295, 311, 158, 28);
+		panelFelicitacion.add(btnCancelar_1);
+		
+		JPanel panelComunicacion = new JPanel();
+		panelComunicacion.setLayout(null);
+		panelCambiante.add(panelComunicacion);
 		
 		JLabel lblTitulo = new JLabel("TITULO:");
 		lblTitulo.setToolTipText("");
 		lblTitulo.setFont(new Font("Cambria Math", Font.PLAIN, 14));
-		lblTitulo.setBounds(30, 61, 82, 28);
-		panel.add(lblTitulo);
+		lblTitulo.setBounds(30, 81, 82, 28);
+		panelComunicacion.add(lblTitulo);
 		
 		JLabel lblContenido = new JLabel("CONTENIDO:");
 		lblContenido.setToolTipText("");
 		lblContenido.setFont(new Font("Cambria Math", Font.PLAIN, 14));
-		lblContenido.setBounds(10, 155, 82, 28);
-		panel.add(lblContenido);
+		lblContenido.setBounds(20, 198, 82, 28);
+		panelComunicacion.add(lblContenido);
 		
 		JLabel lblDestinatarios = new JLabel("DESTINO:");
 		lblDestinatarios.setToolTipText("");
 		lblDestinatarios.setFont(new Font("Cambria Math", Font.PLAIN, 14));
-		lblDestinatarios.setBounds(30, 249, 73, 28);
-		panel.add(lblDestinatarios);
+		lblDestinatarios.setBounds(29, 313, 73, 28);
+		panelComunicacion.add(lblDestinatarios);
 		
 		JLabel lblNewLabel_1 = new JLabel("COMUNICACI\u00D3N");
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 18));
-		lblNewLabel_1.setBounds(177, 10, 182, 28);
-		panel.add(lblNewLabel_1);
+		lblNewLabel_1.setBounds(225, 31, 182, 28);
+		panelComunicacion.add(lblNewLabel_1);
 		
 		JButton btnGuardar = new JButton("ENVIAR");
 		btnGuardar.setFont(new Font("Tahoma", Font.BOLD, 15));
 		btnGuardar.setBackground(Color.BLACK);
-		btnGuardar.setBounds(89, 310, 158, 28);
-		panel.add(btnGuardar);
+		btnGuardar.setBounds(129, 370, 158, 28);
+		panelComunicacion.add(btnGuardar);
 		
-		JButton btnCanccelar = new JButton("BORRAR");
-		btnCanccelar.setFont(new Font("Tahoma", Font.BOLD, 15));
-		btnCanccelar.setBackground(Color.BLACK);
-		btnCanccelar.setBounds(289, 310, 158, 28);
-		panel.add(btnCanccelar);
+		JButton btnCancelar = new JButton("BORRAR");
+		btnCancelar.setFont(new Font("Tahoma", Font.BOLD, 15));
+		btnCancelar.setBackground(Color.BLACK);
+		btnCancelar.setBounds(329, 370, 158, 28);
+		panelComunicacion.add(btnCancelar);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(122, 313, 376, 26);
+		panelComunicacion.add(textField_2);
 		
 		textField_3 = new JTextField();
 		textField_3.setColumns(10);
-		textField_3.setBounds(123, 249, 376, 26);
-		panel.add(textField_3);
+		textField_3.setBounds(122, 120, 376, 183);
+		panelComunicacion.add(textField_3);
 		
 		textField_4 = new JTextField();
 		textField_4.setColumns(10);
-		textField_4.setBounds(123, 99, 376, 140);
-		panel.add(textField_4);
+		textField_4.setBounds(122, 82, 376, 26);
+		panelComunicacion.add(textField_4);
 		
-		textField_5 = new JTextField();
-		textField_5.setColumns(10);
-		textField_5.setBounds(123, 61, 376, 26);
-		panel.add(textField_5);
 		
 	
 	}
