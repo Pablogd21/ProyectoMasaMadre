230670dd270f2fb75ff4f59c3b78014e8495af25
Alvaro-Dans <alvarodans@hotmail.com>
2021-06-10 18:31:25 +0200

    cambios

--- src/views/Marco_Principal.java
+++ src/views/Marco_Principal.java
@@ -5,10 +5,7 @@ import java.awt.Image;
 import java.awt.Toolkit;
 
 import javax.swing.JFrame;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-
-import controller.Controlador;
 
 
 public class Marco_Principal extends JFrame {
