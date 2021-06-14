5a19a252b39b6bed28e2b31412016b9990159473
Alvaro-Dans <alvarodans@hotmail.com>
2021-06-14 12:21:33 +0200

    vista facturacion

--- src/controller/Controlador.java
+++ src/controller/Controlador.java
@@ -2,9 +2,6 @@ package controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-
 import javax.swing.JOptionPane;
 
 import model.data.Cliente;
