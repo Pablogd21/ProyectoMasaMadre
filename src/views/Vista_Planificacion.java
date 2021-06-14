45cfa0d253b7d71538f2ada76d682904f198a30c
Alvaro-Dans <alvarodans@hotmail.com>
2021-06-14 12:10:47 +0200

    planificacion

--- src/views/Vista_Planificacion.java
+++ src/views/Vista_Planificacion.java
@@ -10,6 +10,9 @@ import javax.swing.JPanel;
 
 import controller.Controlador;
 import javax.swing.border.LineBorder;
+import java.awt.FlowLayout;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class Vista_Planificacion extends JPanel {
 
@@ -17,11 +20,68 @@ public class Vista_Planificacion extends JPanel {
 	private JButton btnModificar;
 	private JButton btnHome;
 	private JPanel panel_resumen;
+	private JLabel lblNewLabel;
+	private JLabel lblNewLabel_1;
+	private JLabel lblNewLabel_2;
+	private JLabel lblNewLabel_3;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	public Vista_Planificacion() {
 		setLayout(null);
 
 		this.setSize(1366, 768);
+		
+		JPanel panel_aniadir = new JPanel();
+		panel_aniadir.setBounds(294, 31, 1000, 662);
+		add(panel_aniadir);
+		panel_aniadir.setLayout(null);
+		
+		lblNewLabel = new JLabel("Buscar Cliente:");
+		lblNewLabel.setBounds(10, 23, 85, 30);
+		panel_aniadir.add(lblNewLabel);
+		
+		lblNewLabel_1 = new JLabel("Fecha Pedido:");
+		lblNewLabel_1.setBounds(10, 64, 85, 30);
+		panel_aniadir.add(lblNewLabel_1);
+		
+		lblNewLabel_2 = new JLabel("Descripci\u00F3n");
+		lblNewLabel_2.setBounds(10, 105, 85, 30);
+		panel_aniadir.add(lblNewLabel_2);
+		
+		lblNewLabel_3 = new JLabel("Importe");
+		lblNewLabel_3.setBounds(10, 406, 46, 14);
+		panel_aniadir.add(lblNewLabel_3);
+		
+		JPanel panel = new JPanel();
+		FlowLayout flowLayout = (FlowLayout) panel.getLayout();
+		flowLayout.setHgap(15);
+		flowLayout.setAlignment(FlowLayout.RIGHT);
+		flowLayout.setVgap(45);
+		panel.setBounds(10, 517, 980, 134);
+		panel_aniadir.add(panel);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		panel.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("New button");
+		btnNewButton_1.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		panel.add(btnNewButton_1);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setBounds(368, 118, 622, 255);
+		panel_aniadir.add(textArea);
+		
+		textField = new JTextField();
+		textField.setBounds(368, 28, 622, 20);
+		panel_aniadir.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(368, 69, 622, 20);
+		panel_aniadir.add(textField_1);
 
 		btnAniadirUsu = new JButton("A\u00F1adir Pedido");
 		btnAniadirUsu.setBackground(Color.BLACK);
@@ -54,10 +114,6 @@ public class Vista_Planificacion extends JPanel {
 			}
 		});
 		panel_home.add(btnHome);
-		
-		JPanel panel_aniadir = new JPanel();
-		panel_aniadir.setBounds(294, 31, 1000, 662);
-		add(panel_aniadir);
 		
 		panel_resumen = new JPanel();
 		panel_resumen.setBounds(294, 31, 1000, 662);
